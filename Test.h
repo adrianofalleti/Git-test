b40cb49ab220b0acf7d87cdb636bb22880600484
Adriano Falleti <adrianofalleti@MacBook-Pro-di-Adriano.local>
2015-08-28 09:10:10 +0200

    added a testclass

---
+++ Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Adriano Falleti on 28/08/15.
+//  Copyright (c) 2015 Adriano Falleti. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Adriano Falleti on 28/08/15.
+//  Copyright (c) 2015 Adriano Falleti. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
